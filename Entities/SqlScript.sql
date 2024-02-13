9a539782d2218c72d31f8b7e64ae2e5f7c3b18b2
Anand <anandbansode12@gmail,com>
2024-02-13 10:30:10 +0530

    Add project files.

---
+++ Entities/SqlScript.sql
@@ -0,0 +1,15 @@
+﻿create database CrudMvcDb
+go
+use CrudMvcDb
+go
+create table Players(
+PlayerID int primary key identity,
+PlayerName varchar(max),
+PlayerAge int,
+PlayerAddress varchar(max),
+PlayerJoiningDate datetime2(7)
+)
+go
+insert into Players values ('Rohit Sharma', 35, 'Mumbai','2007/06/30')
+go
+select * from Players
